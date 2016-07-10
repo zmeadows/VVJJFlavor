7143e3923eefa6f531ecae981f8479a980a9e934
Zachary Meadows <zmeadows@gmail.com>
2016-07-10 18:44:58 -0400

    updated to include new efficiency script

--- plotting/batch_plot_all.sh
+++ plotting/batch_plot_all.sh
@@ -7,3 +7,4 @@ OUTPUT_ROOT_DIR="output/$BASE_NAME"
 python batch_plot_qq_qg_gg_events_control_plots.py $RAW_FILEPATH $OUTPUT_ROOT_DIR
 python batch_plot_qq_qg_gg_events_efficiency_plots.py $RAW_FILEPATH $OUTPUT_ROOT_DIR
 python batch_plot_quark_gluon_jets_control_plots.py $RAW_FILEPATH $OUTPUT_ROOT_DIR
+python batch_plot_quark_gluon_jets_efficiency_plots.py $RAW_FILEPATH $OUTPUT_ROOT_DIR
